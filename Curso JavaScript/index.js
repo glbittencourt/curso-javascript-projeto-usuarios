a033640d60b04715151c8afe8e475d0d5b494644
glbittencourt <glbittencourt26@gmail.com>
2025-01-16 16:07:11 -0300

    nova pasta js

---
+++ Curso JavaScript/index.js
@@ -0,0 +1,53 @@
+/**
+ * Comentários
+ * em
+ * todas as
+ * linhas
+ * automaticamente
+ */
+/*var olaMundo = "Hello World!"
+
+console.log(olaMundo);
+console.log(olaMundo);
+console.log(olaMundo);
+console.log(olaMundo);*/
+
+/*let a = 10;
+const b = "10";
+
+console.log(a == b); //true
+console.log(a === b); //false
+
+console.log(a !== b); //true
+console.log(a != b); //false
+
+console.log(a == b && typeof b == 'string'); //as duas verdadeiras
+console.log(a == b || typeof a == 'string'); //um ou outro verdade*/
+
+/*let cor = "amarelo";
+
+if (cor === "verde") {
+
+    console.log("Siga");
+
+} else if (cor === "amarelo") {
+
+    console.log("Cuidado");
+    
+} else {
+
+    console.log("Pare!");
+
+}*/
+
+let n = 5;
+
+for (let i = 0; i <= 10; i++) {
+
+    console.log(`${i} X ${n} = ${i*n}`);
+    //console.log(i + " X " + n + " = " + (i * n));
+    
+
+}
+
+
---
+++ Curso JavaScript/index2.js
@@ -0,0 +1,20 @@
+let cor = "azul";
+
+switch (cor) {
+
+    case "verde":
+      console.log("Siga");
+    break;
+    
+    case "amarelo":
+        console.log("Atenção");
+    break;
+
+    case "vermelho":
+        console.log("PARE!");
+    break;
+
+    default:
+        console.log("não sei");
+
+}
