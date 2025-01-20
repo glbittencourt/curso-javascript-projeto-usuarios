3474acc7c1a6abdaddfe0d8ae0e3975f1c1877f4
glbittencourt <glbittencourt26@gmail.com>
2025-01-20 15:34:17 -0300

    estudo javascript

--- Curso JavaScript/index.js
+++ Curso JavaScript/index.js
@@ -40,7 +40,7 @@ if (cor === "verde") {
 
 }*/
 
-let n = 5;
+/*let n = 5;
 
 for (let i = 0; i <= 10; i++) {
 
@@ -48,6 +48,75 @@ for (let i = 0; i <= 10; i++) {
     //console.log(i + " X " + n + " = " + (i * n));
     
 
+}*/
+
+/*function calc(x1, x2, operator) {
+
+    return eval (`${x1} ${operator} ${x2}`);
+
 }
 
+let resultado = calc(1, 2, "+");
 
+console.log(resultado);*/
+
+/*window.addEventListener('focus', event => {
+
+    console.log("focus");
+});
+
+document.addEventListener('click', event => {
+    console.log('click')
+});*/
+
+/*let agora = new Date();
+
+console.log(agora.toLocaleDateString("pt-br"));*/
+
+/*let carros = ["palio98", "toro", "uno", "marea", 10, true, new Date(), function(){}];
+
+/*console.log(carros[6].getFullYear());*/
+
+/*carros.forEach(function(value, index){
+    console.log(index, value);
+});*/
+
+/*let carros = ["Ferrari", "Lamborghini", "Maseratti", "Jaguar", "McLaren"];
+
+carros.forEach(function(value, index){
+    console.log(index, value);
+
+});*/
+
+/*let celular = function (){
+
+    this.cor = "prata";
+
+    this.ligar = function() {
+        console.log("uma ligação");
+        return "ligando";
+    }
+
+}
+
+let objeto = new celular();
+console.log(objeto.ligar());*/
+
+class celular {
+
+    constructor(){
+
+        this.color = "prata";
+
+    }
+
+    ligar() {
+        console.log("uma ligação");
+        return"ligando";
+    }
+
+}
+
+let objeto = new celular();
+
+console.log(objeto.ligar());
