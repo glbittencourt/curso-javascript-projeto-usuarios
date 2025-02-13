5fc6222bbd3a7845034afa7cf0546753383c7bb0
glbittencourt <glbittencourt26@gmail.com>
2025-02-12 22:58:20 -0300

    atualizaçao

--- Clone/controllers/UserController.js
+++ Clone/controllers/UserController.js
@@ -14,13 +14,25 @@ class UserController {
 
             event.preventDefault();  // Previne o comportamento padrão de envio do formulário
 
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
             let values = this.getValues();  // Obtém os valores preenchidos no formulário
+
+            if (!values) return false;
 
             // Chama a função para obter a foto e aguarda a resposta
             this.getPhoto().then(
                 (content) => {
                     values.photo = content;  // Adiciona o conteúdo da foto aos valores do formulário
+
                     this.addLine(values);  // Adiciona uma linha na tabela com os dados do usuário
+
+                    this.formEl.reset();
+                    
+                    btn.disabled = false;
+
                 },
                 (e) => {
                     console.error(e);  // Caso ocorra erro na obtenção da foto
@@ -64,9 +76,17 @@ class UserController {
     // Método para coletar os valores do formulário
     getValues(){
         let user = {};  // Objeto para armazenar os dados do usuário
+        let isValid = true;
 
         // Itera sobre todos os elementos do formulário
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1  && !field.value ) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             // Trata campos de gênero (checkbox ou radio)
             if (field.name == "gender") {
@@ -75,13 +95,19 @@ class UserController {
                 }
 
             // Para o campo admin, verifica se está marcado (checkbox)
-            } else if (field.name == "admin"){
+            } else if (field.name === "admin"){
                 user[field.name] = field.checked;  // Atribui o valor do checkbox ao objeto user
 
             } else {
                 user[field.name] = field.value;  // Para os outros campos, simplesmente atribui o valor
             }
         });
+
+        if (!isValid) {
+
+            return false;
+
+        }
 
         // Retorna um novo objeto User com os dados coletados
         return new User(
@@ -98,13 +124,14 @@ class UserController {
     // Método para adicionar uma linha na tabela com os dados do usuário
     addLine(dataUser){
         let tr = document.createElement('tr');  // Cria uma nova linha na tabela
+        tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
@@ -112,6 +139,28 @@ class UserController {
         `;  // Preenche a linha com os dados do usuário, incluindo a foto
 
         this.tableEl.appendChild(tr);  // Adiciona a nova linha à tabela
+
+        this.updateCount()
     }
+        
+        updateCount() {
+        
+            let numberUsers = 0;
+            let numberAdmin = 0;
+
+            [...this.tableEl.children].forEach(tr=>{
+
+                numberUsers++;
+
+                let user = JSON.parse(tr.dataset.user)
+
+                if (user.admin) numberAdmin++;
+
+            });
+
+            document.querySelector("#number-users").innerHTML = numberUsers;
+            document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
+        }
 
 }
